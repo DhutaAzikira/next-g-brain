f2cc9463fa39f3ec011e198baa471c46fe3f809f
yovizn <yovizulkarnaen@gmail.com>
2025-07-16 19:35:46 +0700

    update client supabase

--- src/utils/supabase/client.ts
+++ src/utils/supabase/client.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "https://xmzbujijabgznvosqrec.supabase.co";
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "";
 
-export const client = createClient(supabaseUrl!, supabaseKey!);
+export const client = createClient(supabaseUrl, supabaseKey);
